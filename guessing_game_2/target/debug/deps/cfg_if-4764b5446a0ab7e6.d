de8559bac01940d16657508b7132199093a615ed
Toby <87129288+tbysyl@users.noreply.github.com>
2025-10-06 22:49:34 +0100

    Delete guessing_game_2 directory
    Accidentally omitted .gitignore

--- guessing_game_2/target/debug/deps/cfg_if-4764b5446a0ab7e6.d
+++
@@ -1,7 +0,0 @@
-E:\Learning Rust\rust-learning\guessing_game_2\target\debug\deps\cfg_if-4764b5446a0ab7e6.d: C:\Users\Toby\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs
-
-E:\Learning Rust\rust-learning\guessing_game_2\target\debug\deps\libcfg_if-4764b5446a0ab7e6.rlib: C:\Users\Toby\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs
-
-E:\Learning Rust\rust-learning\guessing_game_2\target\debug\deps\libcfg_if-4764b5446a0ab7e6.rmeta: C:\Users\Toby\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs
-
-C:\Users\Toby\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs:
--- guessing_game_2/target/debug/guessing_game_2.d
+++
@@ -1,1 +0,0 @@
-E:\Learning\ Rust\rust-learning\guessing_game_2\target\debug\guessing_game_2.exe: E:\Learning\ Rust\rust-learning\guessing_game_2\src\main.rs
