9999fcf8347aeea9eaa78e272dbbbc628d8b4c0a
tbysyl <toby.sayle@gmail.com>
2025-10-08 01:33:51 +0100

    Practice making a very basic CLI, will try a more advanced one next, then move onto more Rust concepts

---
+++ cli_tool/src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::env;
+
+fn main() {
+    loop {
+        // Skip the first argument (program) and collect the rest of the arguments in vector
+        let argument: Vec<String> = env::args().skip(1).collect();
+        let cmd: &String = &argument[0];
+        let cmd: String = cmd.to_lowercase();
+
+        // Joins all the vector elements and separates with a space
+        let input = argument[1..].join(" ");
+
+        if cmd == "upper" {
+            println!("{}", input.to_uppercase());
+            break
+        } else if cmd == "lower" {
+            println!("{}", input.to_lowercase());
+            break
+        } else {
+            continue
+        }
+    }
+ 
+}
